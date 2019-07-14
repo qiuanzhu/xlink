3df230b242dc79162d70e92ed4efec73f1e88631
qiuanzhu <50160931+qiuanzhu@users.noreply.github.com>
2019-07-13 23:58:37 -0400

    Add files via upload

--- R/MAF.R
+++ R/MAF.R
@@ -9,9 +9,10 @@
 #' @param data a dataset.
 MAF <- function(snp, gender, male, MAF_v, data) {
     data <- stats::na.omit(data[, which(colnames(data) %in% c(snp, gender))])
-    n <- length(data$snp)
-    ind <- (data$gender == male)
-    MAF_R <- (2 * sum(data$snp[ind] != 0) + 2 * sum(data$snp[!ind] == 2) + 1 * sum(data$snp[!ind] == 1))/(2 * n)
+    snp_v <- data[,which(colnames(data) %in% snp)] 
+    n <- length(snp_v)
+    ind <- (data[,which(colnames(data) %in% gender)] == male)
+    MAF_R <- (2 * sum(snp_v[ind] != 0) + 2 * sum(snp_v[!ind] == 2) + 1 * sum(snp_v[!ind] == 1)) /(2 * n)
     if (MAF_R >= MAF_v) {
         return(c(snp, MAF_R))
     } else {
---
+++ cran-comments.md
@@ -0,0 +1,19 @@
+## Test environments
+* local OS X install, R 3.7.0
+* ubuntu 14.04 (on travis-ci), R 3.7.0
+* win-builder (devel and release)
+
+## R CMD check results
+
+0 errors | 0 warnings | 0 note
+
+## R-hub check results 
+
+There was 1 note:
+
+Possibly mis-spelled words in DESCRIPTION:
+  Hao (26:70)
+  Meiling (26:62)
+  Xu (26:58)
+
+* Those are authors' names, which are identified as mis-spelled words. 
